b442816a9ed80126697f0a2f2fa9159e477b7e49
Jonathan Bateman <jondeveloper0@gmail.com>
2023-01-19 12:20:09 -0500

    This first function simulates throwing the die 100 times.

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 import numpy as np
+from matplotlib import pyplot as plt
+
+# simulating rolling the die 100 times
+def single_sim():
+    track_steps = []
+    for i in range(100):
+        step = 0
+        die = np.random.randint(1,7)
+        if die <= 2:
+            max(0,step - 1) # ensures that person can't go below step 0
+        elif die < 6:
+            step += 1
+        else:
+            step = step + np.random.randint(1,7)
+        track_steps.append(step)
+    
+    return track_steps
+
+def main():
+    track_steps = single_sim()
+    print(track_steps)
+
+if __name__ == "__main__":
+    main()
+
+
+
 
